21439d2e3dd96307c1c00aef87a90cefb940f523
nanobee <30323087+nanobee@users.noreply.github.com>
2017-10-22 15:57:26 +0100

    create trierVicteur

--- VectorHelper.java
+++ VectorHelper.java
@@ -1,5 +1,30 @@
 /**
-*this is the main class
-*
-*
-*/
+     * @param vect c'est un tableau d'entiers non trié
+     * @return tableau d'entiers trié
+     */
+   /* public int[] lireVect(int taille){
+        int [] vect = new int[taille];
+        
+        return vect;
+    }*/
+    
+    
+    public static int [] trierVect(int vect[] ){
+    
+    int [] result = vect;
+    int i=0,j=0,temp=0;
+    
+    for(i = 0; i < result.length -1;i++)
+    {
+        j =i ; 
+        while((result[j]>result[j+1]) && (j>0)){
+            temp = result[j];
+            result[j] = result[j+1];
+            result[j+1]=temp;
+            j--;
+        }
+    }
+    
+    return result;
+
+    }
