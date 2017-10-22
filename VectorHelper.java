7830ff5b366f9f2bc960650bdefcea132321d0b5
SidaliMahmoudi <33000957+SidaliMahmoudi@users.noreply.github.com>
2017-10-22 15:52:27 +0100

    j'ai définer le class

--- VectorHelper.java
+++ VectorHelper.java
@@ -1,5 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp;
+
+
 /**
-*this is the main class
-*
-*
-*/
+ *
+ * @author Sid Ali Mahmoudi
+ * @author Merwan Hellal
+ * @version 1.0.0
+ * cette classe sert à effectuer qlq opérations sur les vecteurs comme le trie, la somme le minimum, le maximum ... 
+ */
+
+//cette classe sert à effectuer qlq opérations sur les vecteurs comme le trie, la somme le minimum, le maximum ... 
+public class VecteurHelper {
+
+
+
+}
